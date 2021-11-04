fce317f099b69f5df3c6e386db18b03302cfa165
Jerome Ysrael Endaya <76027883+jeromeslash83@users.noreply.github.com>
2021-11-04 21:13:14 +0800

    Update and rename nothing.py to 15.2.py

---
+++ PY4E exer/15.2.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    org = email.split('@')
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org[1],))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org[1],))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org[1],))
+
+conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
